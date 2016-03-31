fe436d34b04601089f2d9dc92be16bb1cedc309a
Natalia Osiecka <nzw.natalia.osiecka@gmail.com>
2016-03-31 16:36:53 +0200

    loading csv file

--- 2DIR.py
+++ 2DIR.py
@@ -3,9 +3,22 @@
 Natalia
 
 This is a script file for calculating two dimensional correlation analysis.
-Data are louded from the csv file.
+Data are loaded from the csv file.
 Corrections are applied.
 Moving window analysis is made.
 Synchronous and asynchronous spectra are calculated.
 """
+import numpy
 
+filecsv = "6ba12_01.csv"
+
+#load data from the file
+def load_datacsv(filecsv):
+    csvarray = numpy.loadtxt(fname = filecsv, delimiter = ',')
+    return csvarray
+    
+def set_intensity_array(csvarray):
+    intensity_matrix = csvarray[1:,:]
+    return intensity_matrix
+    
+csvarray = load_datacsv(filecsv)
