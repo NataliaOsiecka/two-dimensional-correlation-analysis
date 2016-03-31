830a93f56dba8c6f8360f6ffae4fa4f4d628eb7f
Natalia Osiecka <nzw.natalia.osiecka@gmail.com>
2016-03-31 20:04:13 +0200

    ploting IR spectra

--- 2DIR.py
+++ 2DIR.py
@@ -16,18 +16,31 @@ filecsv = "6ba12_01.csv"
 #load IR data from the file
 def load_datacsv(filename):
     csvarray = numpy.loadtxt(fname = filename, delimiter = ',')
-    intensity = csvarray[1:,:]
-    wavenumber = csvarray[0,:]
+    wavenumber = csvarray[:,0]    
+    intensity, wavenumbers = cut_data(csvarray[:,1:], wavenumber)
     return intensity, wavenumber
+    
+#taking data only in the region of near infrared    
+def cut_data(intensities, wavenumbers):
+    low_index = lower_index(wavenumbers,500)
+    #high_index = high_index(wavenumbers,3500)
+    new_matrix = intensities[low_index:,:]
+    new_wavenumbers = wavenumbers[low_index:,:] 
+    return new_matrix, new_wavenumbers
+    
+def lower_index(some_vector, number):
+    some_list = list(some_vector)
+    for item in some_list:
+        if item > number:
+            return some_list.index(item)
     
 intensity, wavenumber = load_datacsv(filecsv)
 
-print(intensity.shape)
-intensity.T
-print(intensity.shape)
+#transpose intensity matrix for ploting the data
+trans_int = intensity.T
 
-"""
-with plt.style.context('fivethirtyeight'):
-    for item in intensity:
-        plt.plot(wavenumber, item)
-    """
+#ploting IR spectra
+for item in trans_int:
+    plt.plot(wavenumber, item)
+    
+plt.show()
