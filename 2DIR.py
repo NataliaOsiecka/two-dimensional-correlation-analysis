174337e6626a4ec3a93babe33a33ed97ea4e6b1f
Natalia Osiecka <nzw.natalia.osiecka@gmail.com>
2016-03-31 20:52:19 +0200

    cutting end of absorbtion

--- 2DIR.py
+++ 2DIR.py
@@ -14,27 +14,28 @@ import matplotlib.pyplot as plt
 filecsv = "6ba12_01.csv"
 
 #load IR data from the file
-def load_datacsv(filename):
-    csvarray = numpy.loadtxt(fname = filename, delimiter = ',')
-    wavenumber = csvarray[:,0]    
-    intensity, wavenumbers = cut_data(csvarray[:,1:], wavenumber)
-    return intensity, wavenumber
+def load_data(filename):
+    csvarray = numpy.loadtxt(fname = filename, delimiter = ',')   
+    intensity, wavenumber = cut_data(csvarray[:,1:], csvarray[:,0]) 
+    return wavenumber, intensity
     
 #taking data only in the region of near infrared    
 def cut_data(intensities, wavenumbers):
     low_index = lower_index(wavenumbers,500)
     #high_index = high_index(wavenumbers,3500)
-    new_matrix = intensities[low_index:,:]
-    new_wavenumbers = wavenumbers[low_index:,:] 
-    return new_matrix, new_wavenumbers
+    new_intens = intensities[low_index:,:]
+    new_wavenu = wavenumbers[low_index:]
+    return new_intens, new_wavenu
+    
     
 def lower_index(some_vector, number):
     some_list = list(some_vector)
     for item in some_list:
         if item > number:
             return some_list.index(item)
-    
-intensity, wavenumber = load_datacsv(filecsv)
+
+wavenumber, intensity = load_data(filecsv)
+
 
 #transpose intensity matrix for ploting the data
 trans_int = intensity.T
