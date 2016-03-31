55ee0e0ad5c064259fcb10359877421310e2feb3
Natalia Osiecka <nzw.natalia.osiecka@gmail.com>
2016-03-31 15:33:01 +0200

    my 2DIR script, first commit

---
+++ 2DIR.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Natalia
+
+This is a script file for calculating two dimensional correlation analysis.
+Data are louded from the csv file.
+Corrections are applied.
+Moving window analysis is made.
+Synchronous and asynchronous spectra are calculated.
+"""
+
