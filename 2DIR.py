53d23faefc74b091cfb84fae85fbf9b151200506
Natalia Osiecka <nzw.natalia.osiecka@gmail.com>
2016-03-31 17:37:06 +0200

    loading csv file

--- 2DIR.py
+++ 2DIR.py
@@ -9,16 +9,25 @@ Moving window analysis is made.
 Synchronous and asynchronous spectra are calculated.
 """
 import numpy
+import matplotlib.pyplot as plt
 
 filecsv = "6ba12_01.csv"
 
-#load data from the file
-def load_datacsv(filecsv):
-    csvarray = numpy.loadtxt(fname = filecsv, delimiter = ',')
-    return csvarray
+#load IR data from the file
+def load_datacsv(filename):
+    csvarray = numpy.loadtxt(fname = filename, delimiter = ',')
+    intensity = csvarray[1:,:]
+    wavenumber = csvarray[0,:]
+    return intensity, wavenumber
     
-def set_intensity_array(csvarray):
-    intensity_matrix = csvarray[1:,:]
-    return intensity_matrix
-    
-csvarray = load_datacsv(filecsv)
+intensity, wavenumber = load_datacsv(filecsv)
+
+print(intensity.shape)
+intensity.T
+print(intensity.shape)
+
+"""
+with plt.style.context('fivethirtyeight'):
+    for item in intensity:
+        plt.plot(wavenumber, item)
+    """
