8f72f0284b07c8837f53be08446cb7559e703825
Natalia Osiecka <nzw.natalia.osiecka@gmail.com>
2016-04-01 09:44:03 +0200

    reading IR data in near infraread region

--- 2DIR.py
+++ 2DIR.py
@@ -12,6 +12,7 @@ import numpy
 import matplotlib.pyplot as plt
 
 filecsv = "6ba12_01.csv"
+#filetxt = "6ba12_01.txt"
 
 #load IR data from the file
 def load_data(filename):
@@ -22,23 +23,38 @@ def load_data(filename):
 #taking data only in the region of near infrared    
 def cut_data(intensities, wavenumbers):
     low_index = lower_index(wavenumbers,500)
-    #high_index = high_index(wavenumbers,3500)
-    new_intens = intensities[low_index:,:]
-    new_wavenu = wavenumbers[low_index:]
+    high_index = len(list(wavenumbers)) - higher_index(wavenumbers,3500)
+    new_intens = intensities[low_index:high_index,:]
+    new_wavenu = wavenumbers[low_index:high_index]
     return new_intens, new_wavenu
     
     
-def lower_index(some_vector, number):
-    some_list = list(some_vector)
-    for item in some_list:
+def lower_index(wavenumber_vector, number):
+    wavenumber_list = list(wavenumber_vector)
+    for item in wavenumber_list:
         if item > number:
-            return some_list.index(item)
+            return wavenumber_list.index(item)
+            
+def higher_index(wavenumber_vector, number):
+    wavenumber_list = list(wavenumber_vector[::-1])
+    for item in wavenumber_list:
+        if item < number:
+            return wavenumber_list.index(item)
+"""          
+#load temperature file
+def load_temp(filename):
+    with open('filename', 'r') as f:
+        temp_data = f.read()
+    assert f.closed
+    return temp_data
+     """   
 
 wavenumber, intensity = load_data(filecsv)
 
-
 #transpose intensity matrix for ploting the data
 trans_int = intensity.T
+
+#temperature = load_temp()
 
 #ploting IR spectra
 for item in trans_int:
