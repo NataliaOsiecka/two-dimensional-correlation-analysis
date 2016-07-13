18dcfe55f65053d773b73f47743101a8697420de
Natalia Osiecka <nzw.natalia.osiecka@gmail.com>
2016-07-13 12:40:19 +0200

    contour ploting

--- corelations.py
+++ corelations.py
@@ -5,25 +5,15 @@ Created on Fri Jul  8 15:37:59 2016
 @author: Natalia
 """
 
-import data
-import correction as crt
 import numpy
 
-filecsv = "6ba12_01.csv"
-
-wavenumber, intensity = data.load_data(filecsv)
-#transpose intensity matrix for ploting the data
-trans_int = intensity.T
-
-int_baseline = crt.baseline(trans_int)
-intensity_without_co2 = crt.removeCO2band(int_baseline, wavenumber)
 
 def calculate_synchronous_spectra(spectra):
     dynamic_spectrum = calculate_dynamic_spectra(spectra)
-    synchronous = (1/spectra.shape[0])*numpy.dot(dynamic_spectrum, numpy.transpose(dynamic_spectrum))
+    synchronous = (1/spectra.shape[0])*numpy.dot(numpy.transpose(dynamic_spectrum), dynamic_spectrum)
     return synchronous
     
-def calculate_asynchronous_spectr(spectra):
+def calculate_asynchronous_spectra(spectra):
     dynamic_spectrum = calculate_dynamic_spectra(spectra)
     hilbert_matrix = calculate_hilbert_matrix(spectra.shape[0])
     asynchronous = (1/spectra.shape[0])*numpy.dot(numpy.dot(numpy.transpose(dynamic_spectrum),hilbert_matrix), dynamic_spectrum)
@@ -55,3 +45,4 @@ def calculate_hilbert_matrix(number_of_spectra):
             hil[i,j] = 1/(numpy.pi*(j-i))
             hil[j,i] = -hil[i,j]
     return hil
+    
